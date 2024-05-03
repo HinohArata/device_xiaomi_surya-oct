2fc1a6dd6d129dcc29fe9b2853a541d8cb653226
HinohArata <mmgcntk@gmail.com>
2024-05-03 22:48:23 +0700

    surya: Drop XiaomiParts

--- device.mk
+++ device.mk
@@ -35,9 +35,6 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# XiaomiParts
-$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
-
 # Inherit surya firmware images
 $(call inherit-product, firmware/xiaomi/surya/Android.mk)
 
@@ -131,10 +128,6 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libpiex_shim
-
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
 
 # Dex/ART optimization
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
