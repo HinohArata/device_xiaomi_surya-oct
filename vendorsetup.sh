fba65c2860dc86e1825a5ca8faa51cb02d2869e3
HinohArata <mmgcntk@gmail.com>
2024-05-05 20:04:30 +0700

    surya: vendorsetup: Clone important repository for building

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+# Clone Repository for Hardware
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+
+# Clone Repository for Vendor
+git clone https://github.com/HinohArata/surya_vendor.git -b lineage-20 vendor/xiaomi/surya
+
+# Clone Repository for Kernel
+git clone https://github.com/SilonT-Next-GEN/kernel_xiaomi_surya.git -b 4.14-old kernel/xiaomi/surya
+
+# Clone Repository for Firmware
+git clone https://gitlab.com/HinohArata/firmware_xiaomi_surya.git firmware/xiaomi/surya
+
+# Clone Repository for Leica device
+git clone https://gitlab.com/HinohArata/device_xiaomi_surya-miuicamera.git device/xiaomi/surya-miuicamera
+
+# Clone Repository for Leica vendor
+git clone https://gitlab.com/HinohArata/vendor_xiaomi_surya-miuicamera.git -b main vendor/xiaomi/surya-miuicamera
